62facb2cf9b16181fe518c70524258f9c7f6b5b8
endaye <en@endaye.com>
2026-08-24 01:45:51 +0800

    chore(product): allocate precommit crop build

--- apps/creator-web/test/project_actions.test.ts
+++ apps/creator-web/test/project_actions.test.ts
@@ -84,7 +84,7 @@ function sessionFixture(overrides: Partial<CreatorRuntimeSession> = {}) {
       error_details: {},
       product_build: TEST_PRODUCT_BUILD,
       host_id: "creator-web",
-      host_version: "1.3.6",
+      host_version: "1.4.0",
       platform_version: "0.3.4",
       protocol_version: 1,
       capabilities: {
